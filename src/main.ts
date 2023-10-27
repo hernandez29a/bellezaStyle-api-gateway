5a7838bcbb4d898654cb2c5b323b11633081da97
Gustavo Hernandez <hernandez29a@gmail.com>
2023-10-27 15:35:03 -0400

    wip actualizacion del filtrado de excepciones y tiempo de respuesta de peticion

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
   const logger = new Logger('App_Port');
   app.enableCors();
   app.setGlobalPrefix('api/v1');
